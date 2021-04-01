f9cbd48958145996eb24878ee4550e1e5b15f05f
Joaquin Aldunate <joaquin.aldunate@gmail.com>
2021-04-01 12:29:53 +0300

    merge and resolve conflict

--- src/index.js
+++ src/index.js
@@ -70,6 +70,44 @@ window.demos = {
     "multireso": ()=>pat7(webInspectorInterface),
     "wavefolder": ()=>pat8(webInspectorInterface),
     "rustTest": ()=>pat9(webInspectorInterface),
+    "drummaker": ()=>pat1(webInspectorInterface),
+    "kik":()=>{
+        create(possibleModules.Oscillator,'osc1');
+        create(possibleModules.EnvelopeAttackRelease,'env1');
+        create(possibleModules.EnvelopeAttackRelease,'env2');
+        modules['env1'].connectTo(modules['osc1'].inputs.amplitude);
+        modules['env2'].connectTo(modules['osc1'].inputs.frequency);
+        modules['osc1'].set({
+        'amplitude': 0,
+        'bias': 0,
+        'length': 1,
+        'frequency': 1.7777777777777781,
+        'phase': 0,
+        'shape': 'sin',
+        'nativeProcessor': {
+            'ready': true
+        }
+        });
+        modules['env1'].set({
+        'attack': 0.026961451247165532,
+        'release': 0.337437641723356,
+        'amplitude': 0.457793034018502,
+        'attackShape': 1,
+        'releaseShape': 0.42000000000000015
+        });
+        modules['env2'].set({
+        'attack': 0.011111111111111112,
+        'release': 0.20002267573696147,
+        'amplitude': 200.27912293593687,
+        'attackShape': 0.6799999999999999,
+        'releaseShape': 4.880000000000001
+        });
+        setTimeout(()=>{
+            modules['osc1'].getInterface().autoZoom();
+            modules['env1'].getInterface().autoZoom();
+            modules['env2'].getInterface().autoZoom();
+        },200);
+    }
 }
 
 let hashBefore = window.location.hash;
